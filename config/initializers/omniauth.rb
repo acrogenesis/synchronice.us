63e1fc15953b3e3c95503f8cc35ea72337e123b4
Jose Luis Loya <jl.loya@gmail.com>
2013-10-05 00:16:43 -0500

    Added rvm ruby-version & proper facebook permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '706897132672123', '216460ee28922542ac5a8940488401e3', scope: "email,publish_stream"
+  provider :facebook, '706897132672123', '216460ee28922542ac5a8940488401e3', scope: "friends_photos,publish_stream,status_update,user_friends", :display=>"popup", :image_size => "normal"
+
 end
