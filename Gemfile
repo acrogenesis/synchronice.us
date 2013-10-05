f8ce76781c84ab5b8cb00da587f31ec87dff0868
acrogenesis <adrian.rangel@gmail.com>
2013-10-04 22:46:42 -0500

    finish facebook

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem "twitter-bootstrap-rails"
 
 #fblogin
 gem 'omniauth-facebook'
+gem 'koala'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '706897132672123', '216460ee28922542ac5a8940488401e3'
+  provider :facebook, '706897132672123', '216460ee28922542ac5a8940488401e3', scope: "email,publish_stream"
 end
