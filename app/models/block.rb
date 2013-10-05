4a3fa3b26cb69d3780f89fb255c4d849776db814
Jose Luis Loya <jl.loya@gmail.com>
2013-10-05 03:15:48 -0500

    Models RelationShips Done

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,3 +1,7 @@
 class Block < ActiveRecord::Base
+  self.table_name = "blocks"
   attr_accessible :weekday_id
+
+  has_many :users_blocks, :class=>"UsersBlocks" 
+  has_many :users, :through=>:users_blocks
 end
--- app/models/users_events.rb
+++ app/models/users_events.rb
@@ -1,4 +1,7 @@
 class UsersEvents < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :user_id, :event_id, :uid 
+  belongs_to :user
+  belongs_to :event
+
 end
