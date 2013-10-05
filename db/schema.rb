62d3f6eabd6a3973bd3acdf4665ee7fcc3923788
Jose Luis Loya <jl.loya@gmail.com>
2013-10-05 01:37:15 -0500

    Added weekdays and fixed Events columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131005051931) do
+ActiveRecord::Schema.define(:version => 20131005063359) do
 
   create_table "events", :force => true do |t|
-    t.datetime "start"
-    t.datetime "end"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "weekday_id"
   end
 
   create_table "users", :force => true do |t|
@@ -30,6 +29,12 @@ ActiveRecord::Schema.define(:version => 20131005051931) do
     t.datetime "updated_at",       :null => false
     t.string   "pic"
     t.boolean  "calendar"
+  end
+
+  create_table "weekdays", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
